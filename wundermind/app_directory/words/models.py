8b00a859fac24743dd89f7624e3fc9bc4ed8f0b4
Phillip Walker <pwalker@fordham.edu>
2012-08-14 02:12:50 -0400

    Fix models file to include only necessary code

--- wundermind/app_directory/words/models.py
+++ wundermind/app_directory/words/models.py
@@ -1,9 +1,5 @@
 from mongoengine import *
 
-
-connect('words')
-
-
 '''
 Creating user collection
 Fields to be filled in by facebook
@@ -23,22 +19,6 @@ class Words(Document):
     wm_word = StringField(max_length=150, required=True)
     person = ReferenceField(User, reverse_delete_rule=CASCADE)
 
-
-
-Phil = User(first_name = 'Phillip', last_name='Walker', email='pwalker@fordham.edu', gender='M', access_token='eufh234jh4fjh3843ufhi4n3irfri3')
-Phil.save()
-
-
-Wordphil = Words(wm_word="Angry", person = Phil)
-Wordphil.save()
-
-
-for info in User.objects:
-    print info.first_name
-    print info.last_name
-    print info.email
-    print info.gender
-    print info.access_token
 
 
 
